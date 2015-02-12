1d1f159ebc72cdd5c78e40c31a8fa212e508d74a
f.luo <f.luo@Ctrip.com>
2015-02-12 15:26:40 +0800

    add

---
+++ Apocalypsey_s_Blog/Apocalypsey_s_Blog/ViewModels/ArticleViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Apocalypsey_s_Blog.ViewModels
+{
+    public class ArticleViewModel
+    {
+        //标题
+        public string WebTitle { get; set; }
+
+        //路径
+        public string WebPath { get; set; }
+
+        //自然排名
+        public string Seo { get; set; }
+
+
+    }
+}
---
+++ Apocalypsey_s_Blog/Apocalypsey_s_Blog/ViewModels/IndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Apocalypsey_s_Blog.ViewModels
+{
+    public class IndexViewModel
+    {
+
+
+        public int ArticleCount { get; set; }
+        public int NoteCount { get; set; }
+    }
+}
